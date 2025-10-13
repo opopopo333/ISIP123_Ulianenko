66f42ac6f2085af8819b07e7dd105de4589c2de8
opopopo333 <stronzzzz@yandex.ru>
2025-10-13 12:57:51 +0300

    Делам классы курсов и университета где добавляем методы добавления студентов и учителей и создания курса

--- PPPP/Program.cs
+++ PPPP/Program.cs
@@ -59,8 +59,8 @@ namespace UniversityManagement
 
     class Course
     {
-        public string Title { get; private set; }
-        public Teacher Instructor { get; private set; }
+        public string Title;
+        public Teacher Instructor;
         private List<Student> students = new List<Student>();
 
         public Course(string title, Teacher instructor)
